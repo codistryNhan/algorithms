79d383a20dcdab1edff76667e0112705da6a4f83
nhan <nhan@codistry.io>
2017-06-26 20:57:59 -0700

    Updated Insertion sort with helper methods

--- 3_insertionSort.js
+++ 3_insertionSort.js
@@ -1,36 +1,23 @@
-var array = createRandomArray();
+let helper = require('./helper');
 
-printArray(array);
+var array = helper.createRandomArray(10);
+
+helper.printArray(array);
 insertionSort(array);
 
+//  Insertion Sort
+//  Worst: O(n^2)
+//  Insertion > Bubble > Selection
 function insertionSort(array){
   for(var i = 0; i < array.length - 1; i++){
     for(var j = i + 1; j > 0; j--){
       if(array[j] < array[j-1]){
-        swap(array, j, j-1);
+        helper.swap(array, j, j-1);
       } else {
         break;
       }
-      printArray(array);
+      helper.printArray(array);
     }
   }
 }
 
-function createRandomArray(){
-  var array = [];
-  for(var i = 0; i < 10; i++){
-    array[i] = Math.floor(Math.random() * 100);
-  }
-
-  return array;
-}
-
-function printArray(array){
-  console.log(array.toString());
-}
-
-function swap(array, i, j){
-  temp = array[i];
-  array[i] = array[j];
-  array[j] = temp;
-}
