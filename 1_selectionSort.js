b399109632e44bcdb8592bc99d80e6c11b5354c5
nhan <nhan@codistry.io>
2017-06-26 19:09:17 -0700

    Updated sorts with helper methods, and added help module

--- 1_selectionSort.js
+++ 1_selectionSort.js
@@ -1,45 +1,26 @@
-//Selection Sort Algorithm
-//O(N^2)
+const helper =  require('./helper');
 
-var array = createRandomArray();
+let array = helper.createRandomArray(10);
 
-console.log("Starting array");
-printArray(array);
+helper.printArray(array);
 
 selectionSort(array);
 
+
+//  Selection Sort
+//  Worst: O(n^2)
+//  Best: O(n^2)
+//  Comments: Never use
 function selectionSort(array){
   for(var i = 0; i < array.length; i++){
     for(var j = i + 1; j < array.length; j++){
       if(array[i] > array[j]){
-        swap(array, i, j);
+        helper.swap(array, i, j);
       }
 
-      printArray(array);
+      helper.printArray(array);
     }
   }
 }
 
-//Swaps elements
-function swap(array, a, b){
-  var temp = array[a];
-  array[a] = array[b];
-  array[b] = temp;
 
-}
-
-//outputs an array
-function printArray(array){
-  console.log(array.toString());
-}
-
-
-//Creates an array of 10 random elements
-function createRandomArray(){
-  var array = [];
-  for(var i = 0; i < 10; i++){
-    array[i] = Math.floor(Math.random() * 100);
-  }
-
-  return array;
-}
--- 2_bubbleSort.js
+++ 2_bubbleSort.js
@@ -1,20 +1,23 @@
-//Bubble Sort Algorithm
-// O(N^2)
-// A bit faster than Selection sort because adaptive
+let helper = require('./helper');
 
-var array = createRandomArray();
+let array = helper.createRandomArray(10);
 
-printArray(array);
+helper.printArray(array);
+
 bubbleSort(array);
 
+//  Bubble Sort
+//  Worst:O(n^2)
+//  Best:O(N)
+//  Comments: Better than Selection Sort because it is adaptive
 function bubbleSort(array){
   for(var i = 0; i < array.length; i++){
     var swapped = false;
     for(var j = array.length - 1; j > i; j--){
       if(array[j] < array[j - 1]){
-        swap(array, j, j - 1);
+        helper.swap(array, j, j - 1);
         swapped = true;
-        printArray(array);
+        helper.printArray(array);
       }
     }
     if(!swapped){
@@ -23,22 +26,3 @@ function bubbleSort(array){
   }
 }
 
-function printArray(array){
-  console.log(array.toString());
-}
-
-function swap(array, a, b){
-  var temp = array[a];
-  array[a] = array[b];
-  array[b] = temp;
-}
-
-function createRandomArray(){
-  var array = [];
-
-  for(var i = 0; i < 10; i ++){
-    array[i] = Math.floor(Math.random() * 100);
-  }
-
-  return array;
-}
