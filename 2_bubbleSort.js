3f31e0184ceeba04963f37af528ce0f8817b4a2d
nhan <nhan@codistry.io>
2017-06-26 21:05:49 -0700

    Updated Bubble Sort

--- 2_bubbleSort.js
+++ 2_bubbleSort.js
@@ -10,19 +10,19 @@ bubbleSort(array);
 //  Worst:O(n^2)
 //  Best:O(N)
 //  Comments: Better than Selection Sort because it is adaptive
+
 function bubbleSort(array){
-  for(var i = 0; i < array.length; i++){
-    var swapped = false;
-    for(var j = array.length - 1; j > i; j--){
-      if(array[j] < array[j - 1]){
-        helper.swap(array, j, j - 1);
+  for(let i = 0; i < array.length; i++){
+    let swapped = false;
+    for(let j = array.length - 1; j > i; j--){
+      if(array[j] < array[j-1]){
+        helper.swap(array, j, j-1);
         swapped = true;
         helper.printArray(array);
       }
     }
-    if(!swapped){
+    if(swapped == false){
       break;
     }
   }
 }
-
